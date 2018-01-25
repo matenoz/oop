2dd228d668eb03ad8d0332ee2af5e77390115677
matenoz <matenino@gmail.com>
2018-01-25 10:48:22 +0100

    script

---
+++ script.php
@@ -0,0 +1,20 @@
+<?php
+$dayOfTheWeek = array(
+    "lun"  => 7,
+    "mar"  => 4.5,
+    "mer"  => 6,
+    "gio"  => -2,
+    "ven"  => 3,
+    "sab"  => 2.8,
+    "dom"  => -0.7
+);
+
+function average($array){
+    $average;
+    foreach($array as $key => $value){
+        $average = $average + $value/sizeof($array); 
+    }
+    return $average;
+}
+
+echo average($dayOfTheWeek);
